d5c4440262d25e92d49665291e949369ab93c2f3
Marceau <marceaurodrigues@adrar-formation.com>
2023-06-02 01:37:54 +0200

    Début dynamisme des fichiers générés

--- public/mail.php
+++ public/mail.php
@@ -18,3 +18,52 @@ $req = $db->prepare("SELECT nom, prenom, duree_stage, sigle, DATE_FORMAT(date_de
 $req->bindParam(":id_stagiaire", $_GET['id']);
 $req->execute();
 $stage = $req->fetch(PDO::FETCH_ASSOC);
+
+$req = $db->prepare("SELECT nom, prenom, mail, signature, carte
+                    FROM formateurs 
+                    WHERE id=1;");
+$req->execute();
+$formateur = $req->fetch(PDO::FETCH_ASSOC);
+
+$req = $db->prepare("SELECT nom_document, lien, textes_ajoutes
+                    FROM documents 
+                    JOIN documents_pages ON documents.id = documents_pages.id_document
+                    WHERE nom_document='Convention de stage';");
+$req->execute();
+$document = $req->fetchAll(PDO::FETCH_ASSOC);
+
+$html2pdf = new TCPDF('P', 'mm', 'A4', true, 'UTF-8');
+$html2pdf->SetCreator('Marceau RODRIGUES');
+$html2pdf->SetAuthor('Marceau RODRIGUES');
+$html2pdf->SetTitle($document[0]['nom_document']);
+$html2pdf->SetSubject($document[0]['nom_document']);
+
+// var_dump(json_decode($document[0]['textes_ajoutes'], true));
+// die;
+foreach ($document as $page) {
+    $html2pdf->AddPage();
+    $html2pdf->Image(__DIR__ . "/../src/v/templates_documents/" . $page['lien']); // Le template de la convention
+    // $html2pdf->Image(__DIR__ . "/../v/templates_documents/" . $page['lien']); // Le template de la convention
+    if (!empty($page['textes_ajoutes'])) {
+        foreach (json_decode($page['textes_ajoutes'], true) as $i) {
+            $html2pdf->setXY($i['X'], $i['Y']);
+            if(str_contains($i['texte'], "|")) {
+                $texte = explode('|', $i['texte']);
+                $html2pdf->Cell(0, 10, ucwords($stage[$texte[0]]) . " " . strtoupper($stage[$texte[1]]), 0, 1);
+            } elseif(array_key_exists($i['texte'], $stage)) {
+                $html2pdf->Cell(0, 10, $stage[$i['texte']], 0, 1);
+            } elseif(array_key_exists($i['texte'], $formateur)) {
+                $html2pdf->Cell(0, 10, $formateur[$i['texte']], 0, 1);
+            } else {
+                $html2pdf->Cell(0, 10, $i['texte'], 0, 1);
+            }
+            if (isset($i['tampon']) && !empty($i['tampon'])) {
+                $html2pdf->Image(__DIR__ . "/../src/" . $i['tampon'], $i['xtampon'], $i['ytampon'], 60, 30);
+            }
+            if (isset($i['signature']) && !empty($i['signature'])) {
+                $html2pdf->Image(__DIR__ . "/../src/" . $i['signature'], $i['xsignature'], $i['ysignature'], 47, 27.5);
+            }
+        }
+    }
+}
+$html2pdf->Output(__DIR__ . '/../v/tmp/convention.pdf', 'I');

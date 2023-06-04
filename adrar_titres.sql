6be2670ccef862ec318a720f43b53c10202e24a5
MarceauAdrar <marceaurodrigues@adrar-formation.com>
2023-06-04 09:52:45 +0200

    BDD mise à jour

--- adrar_titres.sql
+++ adrar_titres.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Hôte : localhost
--- Généré le : jeu. 01 juin 2023 à 20:48
+-- Généré le : dim. 04 juin 2023 à 07:06
 -- Version du serveur : 10.5.19-MariaDB-0+deb11u2
 -- Version de PHP : 8.2.6
 
@@ -28,7 +28,8 @@ SET time_zone = "+00:00";
 --
 
 CREATE TABLE `documents` (
-  `id` int(11) NOT NULL,
+  `id_document` int(11) NOT NULL,
+  `index_document` varchar(20) NOT NULL,
   `nom_document` varchar(50) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
@@ -36,11 +37,11 @@ CREATE TABLE `documents` (
 -- Déchargement des données de la table `documents`
 --
 
-INSERT INTO `documents` (`id`, `nom_document`) VALUES
-(1, 'Convention de stage'),
-(2, 'Attestation de stage'),
-(3, 'Évaluation de stage'),
-(4, 'Heures entreprises');
+INSERT INTO `documents` (`id_document`, `index_document`, `nom_document`) VALUES
+(1, 'convention', 'Convention de stage'),
+(2, 'attestation', 'Attestation de stage'),
+(3, 'evaluation', 'Évaluation de stage'),
+(4, 'presence', 'Heures entreprises');
 
 -- --------------------------------------------------------
 
@@ -52,8 +53,47 @@ CREATE TABLE `documents_pages` (
   `id` int(11) NOT NULL,
   `num_page` int(11) NOT NULL,
   `lien` varchar(100) NOT NULL,
+  `textes_ajoutes` longtext CHARACTER SET utf8mb4 COLLATE utf8mb4_bin DEFAULT NULL CHECK (json_valid(`textes_ajoutes`)),
   `id_document` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Déchargement des données de la table `documents_pages`
+--
+
+INSERT INTO `documents_pages` (`id`, `num_page`, `lien`, `textes_ajoutes`, `id_document`) VALUES
+(11, 1, 'convention-1.png', '{\n  \"1\": {\n    \"X\": \"50\",\n    \"Y\": \"69\",\n    \"texte\": \"rue_lieu_stage\"\n  },\n  \"2\": {\n    \"X\": \"50\",\n    \"Y\": \"77\",\n    \"texte\": \"cp_lieu_stage\"\n  },\n  \"3\": {\n    \"X\": \"50\",\n    \"Y\": \"85\",\n    \"texte\": \"ville_lieu_stage\"\n  },\n  \"4\": {\n    \"X\": \"127\",\n    \"Y\": \"125.5\",\n    \"texte\": \"nom_tuteur|prenom_tuteur\"\n  },\n  \"5\": {\n    \"X\": \"106\",\n    \"Y\": \"133\",\n    \"texte\": \"mail_tuteur\"\n  },\n  \"6\": {\n    \"X\": \"38\",\n    \"Y\": \"174\",\n    \"texte\": \"nom_stagiaire|prenom_stagiaire\"\n  },\n  \"7\": {\n    \"X\": \"91\",\n    \"Y\": \"210\",\n    \"texte\": \"duree_stage\"\n  },\n  \"8\": {\n    \"X\": \"58.5\",\n    \"Y\": \"218.5\",\n    \"texte\": \"date_debut_stage\"\n  },\n  \"9\": {\n    \"X\": \"108\",\n    \"Y\": \"218.5\",\n    \"texte\": \"date_fin_stage\"\n  },\n  \"10\": {\n    \"X\": \"26.5\",\n    \"Y\": \"238\",\n    \"texte\": \"nom_formateur|prenom_formateur\"\n  },\n  \"11\": {\n    \"X\": \"85\",\n    \"Y\": \"238\",\n    \"texte\": \"mail_formateur\"\n  }\n}', 1),
+(12, 2, 'convention-2.png', NULL, 1),
+(13, 3, 'convention-3.png', NULL, 1),
+(14, 4, 'convention-4.png', NULL, 1),
+(15, 5, 'convention-5.png', '{\n  \"1\": {\n    \"X\": \"27\",\n    \"Y\": \"125\",\n    \"texte\": \"nom_tuteur|prenom_tuteur\"\n  },\n  \"2\": {\n    \"X\": \"83\",\n    \"Y\": \"125\",\n    \"texte\": \"nom_formateur|prenom_formateur\"\n  },\n  \"3\": {\n    \"X\": \"139\",\n    \"Y\": \"125\",\n    \"texte\": \"nom_stagiaire|prenom_stagiaire\",\n    \"tampon\": \"v/tampon.png\",\n    \"xtampon\": \"75\",\n    \"ytampon\": \"180\",\n    \"signature\": \"v/formateurs/signature_Marceau_RODRIGUES.png\",\n    \"xsignature\": \"85\",\n    \"ysignature\": \"180\"\n  }\n}', 1),
+(16, 1, 'attestation.png', '{\n  \"1\": {\n    \"X\": \"55\",\n    \"Y\": \"45\",\n    \"texte\": \"nom_stagiaire|prenom_stagiaire\"\n  },\n  \"2\": {\n    \"X\": \"94\",\n    \"Y\": \"62\",\n    \"texte\": \"date_debut_stage\"\n  },\n  \"3\": {\n    \"X\": \"94\",\n    \"Y\": \"72\",\n    \"texte\": \"date_fin_stage\"\n  },\n  \"4\": {\n    \"X\": \"50\",\n    \"Y\": \"82\",\n    \"texte\": \"duree_stage\"\n  }\n}', 2),
+(17, 1, 'evaluation-1.png', '{\n  \"1\": {\n    \"X\": \"32\",\n    \"Y\": \"49\",\n    \"texte\": \"nom_stagiaire|prenom_stagiaire\"\n  },\n  \"2\": {\n    \"X\": \"42\",\n    \"Y\": \"56.5\",\n    \"texte\": \"sigle_session\"\n  },\n  \"3\": {\n    \"X\": \"127\",\n    \"Y\": \"56.5\",\n    \"texte\": \"nom_tuteur|prenom_tuteur\"\n  },\n  \"4\": {\n    \"X\": \"56\",\n    \"Y\": \"64.5\",\n    \"texte\": \"date_debut_stage\"\n  },\n  \"5\": {\n    \"X\": \"92\",\n    \"Y\": \"64.5\",\n    \"texte\": \"date_fin_stage\"\n  },\n  \"6\": {\n    \"X\": \"147\",\n    \"Y\": \"64.5\",\n    \"texte\": \"duree_stage\"\n  }\n}', 3),
+(18, 2, 'evaluation-2.png', '{\n  \"1\": {\n    \"X\": \"138\",\n    \"Y\": \"185.5\",\n    \"texte\": \"nom_formateur|prenom_formateur\",\n    \"signature\": \"v/formateurs/signature_Marceau_RODRIGUES.png\",\n    \"xsignature\": \"138\",\n    \"ysignature\": \"187.5\"\n  }\n}', 3),
+(19, 1, 'presence.png', '{\n  \"1\": {\n    \"X\": \"113\",\n    \"Y\": \"38\",\n    \"texte\": \"nom_stagiaire|prenom_stagiaire\"\n  },\n  \"2\": {\n    \"X\": \"50\",\n    \"Y\": \"42.5\",\n    \"texte\": \"sigle_session\"\n  },\n  \"3\": {\n    \"X\": \"122\",\n    \"Y\": \"46\",\n    \"texte\": \"date_debut_session\"\n  },\n  \"4\": {\n    \"X\": \"122\",\n    \"Y\": \"51\",\n    \"texte\": \"date_fin_session\"\n  },\n  \"5\": {\n    \"X\": \"113\",\n    \"Y\": \"211\",\n    \"texte\": \"nom_tuteur|prenom_tuteur\"\n  }\n}', 4);
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `formateurs`
+--
+
+CREATE TABLE `formateurs` (
+  `id_formateur` int(11) NOT NULL,
+  `nom_formateur` varchar(25) NOT NULL,
+  `prenom_formateur` varchar(25) NOT NULL,
+  `mail_formateur` varchar(100) NOT NULL,
+  `signature_formateur` varchar(50) NOT NULL,
+  `carte_formateur` varchar(50) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Déchargement des données de la table `formateurs`
+--
+
+INSERT INTO `formateurs` (`id_formateur`, `nom_formateur`, `prenom_formateur`, `mail_formateur`, `signature_formateur`, `carte_formateur`) VALUES
+(1, 'RODRIGUES', 'Marceau', 'marceaurodrigues@adrar-formation.com', 'v/formateurs/signature_Marceau_RODRIGUES.png', 'v/formateurs/carte_Marceau_RODRIGUES.png'),
+(2, 'HABBAS', 'Emna', 'emnahabbas@adrar-formation.com', 'v/formateurs/signature_Emna_HABBAS.png', 'v/formateurs/carte_Emna_HABBAS.png');
 
 -- --------------------------------------------------------
 
@@ -62,10 +102,12 @@ CREATE TABLE `documents_pages` (
 --
 
 CREATE TABLE `sessions` (
-  `id` int(11) NOT NULL,
+  `id_session` int(11) NOT NULL,
   `nom_session` varchar(25) NOT NULL,
   `duree_stage` int(11) NOT NULL,
-  `sigle` varchar(50) NOT NULL,
+  `sigle_session` varchar(50) NOT NULL,
+  `date_debut_session` date NOT NULL,
+  `date_fin_session` date NOT NULL,
   `date_debut_stage` date NOT NULL,
   `date_fin_stage` date NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
@@ -74,9 +116,9 @@ CREATE TABLE `sessions` (
 -- Déchargement des données de la table `sessions`
 --
 
-INSERT INTO `sessions` (`id`, `nom_session`, `duree_stage`, `sigle`, `date_debut_stage`, `date_fin_stage`) VALUES
-(1, 'DEV34-22-02', 280, 'DWWM', '2023-04-11', '2023-06-09'),
-(2, 'CDA34-23-01', 350, 'CDA', '0000-00-00', '0000-00-00');
+INSERT INTO `sessions` (`id_session`, `nom_session`, `duree_stage`, `sigle_session`, `date_debut_session`, `date_fin_session`, `date_debut_stage`, `date_fin_stage`) VALUES
+(1, 'DEV34-22-02', 280, 'DWWM', '0000-00-00', '0000-00-00', '2023-04-11', '2023-06-09'),
+(2, 'CDA34-23-01', 350, 'CDA', '2023-05-09', '0000-00-00', '0000-00-00', '0000-00-00');
 
 -- --------------------------------------------------------
 
@@ -85,8 +127,11 @@ INSERT INTO `sessions` (`id`, `nom_session`, `duree_stage`, `sigle`, `date_debut
 --
 
 CREATE TABLE `stages` (
-  `id` int(11) NOT NULL,
-  `lieu` varchar(255) NOT NULL,
+  `id_stage` int(11) NOT NULL,
+  `rue_lieu_stage` varchar(100) NOT NULL,
+  `cp_lieu_stage` varchar(5) NOT NULL,
+  `ville_lieu_stage` varchar(50) NOT NULL,
+  `pays_lieu_stage` varchar(25) NOT NULL,
   `nom_tuteur` varchar(50) NOT NULL,
   `prenom_tuteur` varchar(50) NOT NULL,
   `mail_tuteur` varchar(100) NOT NULL
@@ -96,19 +141,19 @@ CREATE TABLE `stages` (
 -- Déchargement des données de la table `stages`
 --
 
-INSERT INTO `stages` (`id`, `lieu`, `nom_tuteur`, `prenom_tuteur`, `mail_tuteur`) VALUES
-(1, '68 avenue du pont Juvenal, 34000 Montpellier, France', 'BALBIS', 'Mouro', 'tatobalbis@gmail.com'),
-(2, '64B route de Narbonne, 34500 Béziers, France', 'BOUJANDIR', 'Adil', 'topspeed.beziers@gmail.com'),
-(3, '4 avenue de la voie domitienne, 34500 Béziers, France', 'GHIOUANE', 'Chafik', 'chafeck@hotmail.fr'),
-(4, '11 Quai Baley, 29150 Chateaulin, France', 'DURAND', 'Jérémy', 'taxidukoala@gmail.com'),
-(5, '2 Boulevard Jean Bouin, 34500 Béziers, France', 'LEPRETRE', 'Emmanuel', 'lesopticiensaccord@gmail.com'),
-(6, '710 rue d\'Aleo, 34080 Montpellier, France', 'BOURAI', 'Ramdane', 'r.bourai@nouas.org'),
-(7, '4 quai de la dunette, 34200 Sète, France', 'BRENAC', 'Philippe', 'brenacphilippe73@gmail.com'),
-(8, '239 rue des Étoffes, 34400 Lunel, France', 'AMIRROUCHE', 'Redouane', 'contact@perform34.com'),
-(9, '11 rue de l\'abrivado, 34740 Vendargues, France', 'GADEA', 'Delphine', 'ehpadmasmarguerite.secretariat@gmail.com'),
-(10, '7 rue de la Costa Brava, 34070 Montpellier, France', 'SQUARATTI', 'Ryan', 'contact@foodeat.fr'),
-(11, '23 avenue de  la mouyssaguese, 31280 Dremil Lafage, France', 'POP', 'Alexandru', 'entreprise.benazet@gmail.com'),
-(12, '2 avenue de Montpellier, 34820 Teyran, France', 'JOUAUX', 'Muriel', 'aquas34@yahoo.com');
+INSERT INTO `stages` (`id_stage`, `rue_lieu_stage`, `cp_lieu_stage`, `ville_lieu_stage`, `pays_lieu_stage`, `nom_tuteur`, `prenom_tuteur`, `mail_tuteur`) VALUES
+(1, '68 Avenue du Pont Juvenal', '34000', 'Montpellier', 'France', 'BALBIS', 'Mouro', 'tatobalbis@gmail.com'),
+(2, '64B route de Narbonne', '34500', 'Béziers', 'France', 'BOUJANDIR', 'Adil', 'topspeed.beziers@gmail.com'),
+(3, '4 Avenue de la voie domitienne', '34500', 'Béziers', 'France', 'GHIOUANE', 'Chafik', 'chafeck@hotmail.fr'),
+(4, '11 Quai Baley', '29150', 'Chateaulin', 'France', 'DURAND', 'Jérémy', 'taxidukoala@gmail.com'),
+(5, '2 Boulevard Jean Bouin', '34500', 'Béziers', 'France', 'LEPRETRE', 'Emmanuel', 'lesopticiensaccord@gmail.com'),
+(6, '710 rue d\'Aleo', '34080', 'Montpellier', 'France', 'BOURAI', 'Ramdane', 'r.bourai@nouas.org'),
+(7, '4 Quai de la dunette', '34200', 'Sète', 'France', 'BRENAC', 'Philippe', 'brenacphilippe73@gmail.com'),
+(8, '239 rue des Étoffes', '34400', 'Lunel', 'France', 'AMIRROUCHE', 'Redouane', 'contact@perform34.com'),
+(9, '11 rue de l\'abrivado', '34740', 'Vendargues', 'France', 'GADEA', 'Delphine', 'ehpadmasmarguerite.secretariat@gmail.com'),
+(10, '7 rue de la Costa Brava', '34070', 'Montpellier', 'France', 'SQUARATTI', 'Ryan', 'contact@foodeat.fr'),
+(11, '23 avenue de la mouyssaguese', '31280', 'Dremil Lafage', 'France', 'POP', 'Alexandru', 'entreprise.benazet@gmail.com'),
+(12, '2 avenue de Montpellier', '34820', 'Teyran', 'France', 'JOUAUX', 'Muriel', 'aquas34@yahoo.com');
 
 -- --------------------------------------------------------
 
@@ -117,9 +162,9 @@ INSERT INTO `stages` (`id`, `lieu`, `nom_tuteur`, `prenom_tuteur`, `mail_tuteur`
 --
 
 CREATE TABLE `stagiaires` (
-  `id` int(11) NOT NULL,
-  `nom` varchar(50) NOT NULL,
-  `prenom` varchar(50) NOT NULL,
+  `id_stagiaire` int(11) NOT NULL,
+  `nom_stagiaire` varchar(50) NOT NULL,
+  `prenom_stagiaire` varchar(50) NOT NULL,
   `id_session` int(11) NOT NULL,
   `id_stage` int(11) DEFAULT NULL,
   `convention_recue` tinyint(1) NOT NULL DEFAULT 0,
@@ -137,7 +182,7 @@ CREATE TABLE `stagiaires` (
 -- Déchargement des données de la table `stagiaires`
 --
 
-INSERT INTO `stagiaires` (`id`, `nom`, `prenom`, `id_session`, `id_stage`, `convention_recue`, `horaires_recues_1`, `horaires_recues_2`, `horaires_recues_3`, `attestation_mail_envoye`, `attestation_recue`, `evaluation_mail_envoye`, `evaluation_recue`, `compteur_demandes`) VALUES
+INSERT INTO `stagiaires` (`id_stagiaire`, `nom_stagiaire`, `prenom_stagiaire`, `id_session`, `id_stage`, `convention_recue`, `horaires_recues_1`, `horaires_recues_2`, `horaires_recues_3`, `attestation_mail_envoye`, `attestation_recue`, `evaluation_mail_envoye`, `evaluation_recue`, `compteur_demandes`) VALUES
 (23, 'BOTTA', 'Facundo', 1, 1, 1, 1, 1, 0, 1, '0', 1, '0', 1),
 (24, 'BOUJANDIR', 'Karim', 1, 2, 1, 1, 0, 0, 1, '0', 1, '0', 1),
 (25, 'BOURHIL', 'Yannis', 1, 3, 1, 0, 0, 0, 1, '0', 1, '0', 1),
@@ -169,7 +214,7 @@ INSERT INTO `stagiaires` (`id`, `nom`, `prenom`, `id_session`, `id_stage`, `conv
 -- Index pour la table `documents`
 --
 ALTER TABLE `documents`
-  ADD PRIMARY KEY (`id`);
+  ADD PRIMARY KEY (`id_document`) USING BTREE;
 
 --
 -- Index pour la table `documents_pages`
@@ -179,22 +224,28 @@ ALTER TABLE `documents_pages`
   ADD KEY `id_document` (`id_document`);
 
 --
+-- Index pour la table `formateurs`
+--
+ALTER TABLE `formateurs`
+  ADD PRIMARY KEY (`id_formateur`);
+
+--
 -- Index pour la table `sessions`
 --
 ALTER TABLE `sessions`
-  ADD PRIMARY KEY (`id`);
+  ADD PRIMARY KEY (`id_session`);
 
 --
 -- Index pour la table `stages`
 --
 ALTER TABLE `stages`
-  ADD PRIMARY KEY (`id`);
+  ADD PRIMARY KEY (`id_stage`);
 
 --
 -- Index pour la table `stagiaires`
 --
 ALTER TABLE `stagiaires`
-  ADD PRIMARY KEY (`id`),
+  ADD PRIMARY KEY (`id_stagiaire`),
   ADD KEY `id_session` (`id_session`),
   ADD KEY `id_stage` (`id_stage`);
 
@@ -206,31 +257,37 @@ ALTER TABLE `stagiaires`
 -- AUTO_INCREMENT pour la table `documents`
 --
 ALTER TABLE `documents`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+  MODIFY `id_document` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
 
 --
 -- AUTO_INCREMENT pour la table `documents_pages`
 --
 ALTER TABLE `documents_pages`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=20;
+
+--
+-- AUTO_INCREMENT pour la table `formateurs`
+--
+ALTER TABLE `formateurs`
+  MODIFY `id_formateur` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 
 --
 -- AUTO_INCREMENT pour la table `sessions`
 --
 ALTER TABLE `sessions`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+  MODIFY `id_session` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 
 --
 -- AUTO_INCREMENT pour la table `stages`
 --
 ALTER TABLE `stages`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=13;
+  MODIFY `id_stage` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=13;
 
 --
 -- AUTO_INCREMENT pour la table `stagiaires`
 --
 ALTER TABLE `stagiaires`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=45;
+  MODIFY `id_stagiaire` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=45;
 
 --
 -- Contraintes pour les tables déchargées
@@ -240,14 +297,14 @@ ALTER TABLE `stagiaires`
 -- Contraintes pour la table `documents_pages`
 --
 ALTER TABLE `documents_pages`
-  ADD CONSTRAINT `documents_pages_ibfk_1` FOREIGN KEY (`id_document`) REFERENCES `documents` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
+  ADD CONSTRAINT `documents_pages_ibfk_1` FOREIGN KEY (`id_document`) REFERENCES `documents` (`id_document`) ON DELETE CASCADE ON UPDATE CASCADE;
 
 --
 -- Contraintes pour la table `stagiaires`
 --
 ALTER TABLE `stagiaires`
-  ADD CONSTRAINT `stagiaires_ibfk_1` FOREIGN KEY (`id_stage`) REFERENCES `stages` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
-  ADD CONSTRAINT `stagiaires_ibfk_2` FOREIGN KEY (`id_session`) REFERENCES `sessions` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
+  ADD CONSTRAINT `stagiaires_ibfk_1` FOREIGN KEY (`id_stage`) REFERENCES `stages` (`id_stage`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `stagiaires_ibfk_2` FOREIGN KEY (`id_session`) REFERENCES `sessions` (`id_session`) ON DELETE CASCADE ON UPDATE CASCADE;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;

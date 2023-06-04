a49665a643480cf448fe582b53186bfc1f6f5c40
MarceauAdrar <marceaurodrigues@adrar-formation.com>
2023-06-04 09:55:35 +0200

    Restructuration complète du code pour être plus modulaire

--- public/mail.php
+++ public/mail.php
@@ -1,69 +1,85 @@
 <?php
 error_reporting(E_ALL);
 ini_set("display_errors", 1);
-//Import PHPMailer classes into the global namespace
-//These must be at the top of your script, not inside a function
-use PHPMailer\PHPMailer\PHPMailer;
-use PHPMailer\PHPMailer\SMTP;
-use PHPMailer\PHPMailer\Exception;
 
 //Load Composer's autoloader
 require __DIR__ . '/../src/vendor/autoload.php';
 require __DIR__ . '/../src/m/connect.php';
-$req = $db->prepare("SELECT nom, prenom, duree_stage, sigle, DATE_FORMAT(date_debut_stage, '%d/%m/%Y') AS date_debut, DATE_FORMAT(date_fin_stage, '%d/%m/%Y') AS date_fin, nom_tuteur, prenom_tuteur, mail_tuteur
+
+$docs = array('convention', 'attestation', 'evaluation', 'presence');
+// $docs = filter_input_array(INPUT_POST, FILTER_SANITIZE_FULL_SPECIAL_CHARS);
+
+$liste_documents = implode("', '", $docs);
+$req = $db->prepare("SELECT id_document, index_document, nom_document
+                    FROM documents
+                    WHERE index_document IN ('" . $liste_documents . "');");
+$req->execute();
+$documents = $req->fetchAll(PDO::FETCH_ASSOC);
+
+$req = $db->prepare("SELECT nom_stagiaire, prenom_stagiaire, duree_stage, sigle_session, DATE_FORMAT(date_debut_session, '%d/%m/%Y') AS date_debut_session, DATE_FORMAT(date_fin_session, '%d/%m/%Y') AS date_fin_session, DATE_FORMAT(date_debut_stage, '%d/%m/%Y') AS date_debut_stage, DATE_FORMAT(date_fin_stage, '%d/%m/%Y') AS date_fin_stage, rue_lieu_stage, cp_lieu_stage, ville_lieu_stage, pays_lieu_stage, nom_tuteur, prenom_tuteur, mail_tuteur
                     FROM stagiaires 
-                    JOIN sessions ON sessions.id = stagiaires.id_session
-                    LEFT JOIN stages ON stages.id = stagiaires.id_stage
-                    WHERE stagiaires.id=:id_stagiaire;");
-$req->bindParam(":id_stagiaire", $_GET['id']);
+                    JOIN sessions ON sessions.id_session = stagiaires.id_session
+                    LEFT JOIN stages ON stages.id_stage = stagiaires.id_stage
+                    WHERE stagiaires.id_stagiaire=:id_stagiaire;");
+$req->bindParam(":id_stagiaire", $_GET['id']); // TODO: rendre cette valeur dynamique
 $req->execute();
 $stage = $req->fetch(PDO::FETCH_ASSOC);
 
-$req = $db->prepare("SELECT nom, prenom, mail, signature, carte
+$req = $db->prepare("SELECT nom_formateur, prenom_formateur, mail_formateur, signature_formateur, carte_formateur
                     FROM formateurs 
-                    WHERE id=1;");
+                    WHERE id_formateur=1;"); // TODO: rendre cette valeur dynamique
 $req->execute();
 $formateur = $req->fetch(PDO::FETCH_ASSOC);
 
-$req = $db->prepare("SELECT nom_document, lien, textes_ajoutes
-                    FROM documents 
-                    JOIN documents_pages ON documents.id = documents_pages.id_document
-                    WHERE nom_document='Convention de stage';");
-$req->execute();
-$document = $req->fetchAll(PDO::FETCH_ASSOC);
+if (!empty($documents)) {
 
-$html2pdf = new TCPDF('P', 'mm', 'A4', true, 'UTF-8');
-$html2pdf->SetCreator('Marceau RODRIGUES');
-$html2pdf->SetAuthor('Marceau RODRIGUES');
-$html2pdf->SetTitle($document[0]['nom_document']);
-$html2pdf->SetSubject($document[0]['nom_document']);
+    foreach ($documents as $document) {
+        $req = $db->prepare("SELECT nom_document, lien, textes_ajoutes
+                            FROM documents 
+                            JOIN documents_pages ON documents.id_document = documents_pages.id_document
+                            WHERE index_document=:index_document;");
+        $req->bindParam(":index_document", $document['index_document']);
+        $req->execute();
+        $pages = $req->fetchAll(PDO::FETCH_ASSOC);
 
-// var_dump(json_decode($document[0]['textes_ajoutes'], true));
-// die;
-foreach ($document as $page) {
-    $html2pdf->AddPage();
-    $html2pdf->Image(__DIR__ . "/../src/v/templates_documents/" . $page['lien']); // Le template de la convention
-    // $html2pdf->Image(__DIR__ . "/../v/templates_documents/" . $page['lien']); // Le template de la convention
-    if (!empty($page['textes_ajoutes'])) {
-        foreach (json_decode($page['textes_ajoutes'], true) as $i) {
-            $html2pdf->setXY($i['X'], $i['Y']);
-            if(str_contains($i['texte'], "|")) {
-                $texte = explode('|', $i['texte']);
-                $html2pdf->Cell(0, 10, ucwords($stage[$texte[0]]) . " " . strtoupper($stage[$texte[1]]), 0, 1);
-            } elseif(array_key_exists($i['texte'], $stage)) {
-                $html2pdf->Cell(0, 10, $stage[$i['texte']], 0, 1);
-            } elseif(array_key_exists($i['texte'], $formateur)) {
-                $html2pdf->Cell(0, 10, $formateur[$i['texte']], 0, 1);
-            } else {
-                $html2pdf->Cell(0, 10, $i['texte'], 0, 1);
+        if (!empty($pages)) {
+            $html2pdf = new TCPDF('P', 'mm', 'A4', true, 'UTF-8');
+            $html2pdf->SetCreator(ucfirst($formateur['prenom_formateur']) . " " . strtoupper($formateur['nom_formateur']));
+            $html2pdf->SetAuthor(ucfirst($formateur['prenom_formateur']) . " " . strtoupper($formateur['nom_formateur']));
+            $html2pdf->SetTitle($pages[0]['nom_document']);
+            $html2pdf->SetSubject($pages[0]['nom_document']);
+
+            foreach ($pages as $page) {
+                $html2pdf->AddPage();
+                $html2pdf->Image(__DIR__ . "/../v/templates_documents/" . $document['index_document'] . "/" . $page['lien']); // Le template de la convention
+                if (!empty($page['textes_ajoutes'])) {
+                    foreach (json_decode($page['textes_ajoutes'], true) as $i) {
+                        $html2pdf->setXY($i['X'], $i['Y']);
+                        if (str_contains($i['texte'], "|")) {
+                            $texte = explode('|', $i['texte']);
+                            if (array_key_exists($texte[0], $stage) || array_key_exists($texte[1], $stage)) {
+                                $html2pdf->Cell(0, 10, strtoupper($stage[$texte[0]]) . " " . ucwords($stage[$texte[1]]), 0, 1);
+                            } elseif (array_key_exists($texte[0], $formateur) || array_key_exists($texte[1], $formateur)) {
+                                $html2pdf->Cell(0, 10, strtoupper($formateur[$texte[0]]) . " " . ucwords($formateur[$texte[1]]), 0, 1);
+                            }
+                        } elseif (array_key_exists($i['texte'], $stage)) {
+                            $html2pdf->Cell(0, 10, $stage[$i['texte']], 0, 1);
+                        } elseif (array_key_exists($i['texte'], $formateur)) {
+                            $html2pdf->Cell(0, 10, $formateur[$i['texte']], 0, 1);
+                        } else {
+                            $html2pdf->Cell(0, 10, $i['texte'], 0, 1);
+                        }
+                        if (isset($i['tampon']) && !empty($i['tampon'])) { // Gère le tampon s'il est nécessaire sur le document
+                            $html2pdf->Image(__DIR__ . "/../" . $i['tampon'], $i['xtampon'], $i['ytampon'], 60, 30);
+                        }
+                        if (isset($i['signature']) && !empty($i['signature'])) { // Gère la signature du formateur si elle est nécessaire sur le document
+                            $html2pdf->Image(__DIR__ . "/../" . $i['signature'], $i['xsignature'], $i['ysignature'], 47, 27.5);
+                        }
+                    }
+                }
             }
-            if (isset($i['tampon']) && !empty($i['tampon'])) {
-                $html2pdf->Image(__DIR__ . "/../src/" . $i['tampon'], $i['xtampon'], $i['ytampon'], 60, 30);
-            }
-            if (isset($i['signature']) && !empty($i['signature'])) {
-                $html2pdf->Image(__DIR__ . "/../src/" . $i['signature'], $i['xsignature'], $i['ysignature'], 47, 27.5);
-            }
+            $html2pdf->Output(__DIR__ . '/../v/tmp/' . $document['index_document'] . '.pdf', 'F');
+            $mail->addAttachment(__DIR__ . '/../v/tmp/' . $document['index_document'] . '.pdf', str_replace(" ", "_", $document['nom_document']) . '.pdf');
         }
     }
 }
-$html2pdf->Output(__DIR__ . '/../v/tmp/convention.pdf', 'I');

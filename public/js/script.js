97bb527b59496ea967bffbe595768e4f4e072a4a
MarceauAdrar <marceaurodrigues@adrar-formation.com>
2023-06-04 21:30:32 +0200

    Ajout de la fonction d'export, renommage de champs, ajout de filtres

--- public/js/script.js
+++ public/js/script.js
@@ -2,20 +2,21 @@
 async function recupererDonnees() {
   var formData = new FormData();
   formData.append('recupererDonnees', 1);
-  
+
   await fetch("../src/c/c_requetes.php", {
     method: "POST",
     body: formData
   })
-  .then((response) => response.json())
-  .then((result) => {
-    document.getElementById("id_formateur").innerHTML = result.formateurs;
-    document.getElementById("tables-stages").innerHTML = result.stages;
+    .then((response) => response.json())
+    .then((result) => {
+      document.getElementById("id_formateur").innerHTML = result.formateurs;
+      document.getElementById("nom_session").innerHTML = result.sessions;
+      document.getElementById("tables-stages").innerHTML = result.stages;
 
-    // Permet de mettre le trigger sur les nouveaux boutons
-  
-    const modals = document.querySelectorAll('[data-modal]');
-    modals.forEach(function (trigger) {
+      // Permet de mettre le trigger sur les nouveaux boutons
+
+      const modals = document.querySelectorAll('[data-modal]');
+      modals.forEach(function (trigger) {
         trigger.addEventListener('click', function (event) {
           event.preventDefault();
           const modal = document.getElementById(trigger.dataset.modal);
@@ -37,7 +38,7 @@ async function recupererDonnees() {
               document.getElementById("relance").value = "0";
             });
           });
-          
+
         });
       });
     });
@@ -49,36 +50,44 @@ async function recupererDocumentsManquants(id_stagiaire) {
   var formData = new FormData();
   formData.append('recupererDocumentsManquants', 1);
   formData.append('id_stagiaire', id_stagiaire);
-  
+
   await fetch("../src/c/c_requetes.php", {
     method: "POST",
     body: formData
   })
-  .then((response) => response.text())
-  .then((result) => {
-    document.getElementById("liste_documents").innerHTML = result;
-  });
+    .then((response) => response.text())
+    .then((result) => {
+      document.getElementById("liste_documents").innerHTML = result;
+    });
 }
 
 async function envoyerMail() {
-    var formData = new FormData();
-    formData.append('envoyerMail', 1);
-    formData.append('id_stagiaire', document.getElementById("id_stagiaire").value);
-    formData.append('id_formateur', document.getElementById("id_formateur").value);
-    if(document.getElementById("convention")) formData.append('convention', document.getElementById("convention").checked);
-    if(document.getElementById("horaires_mois_1")) formData.append('horaires_mois_1', document.getElementById("horaires_mois_1").checked);
-    if(document.getElementById("horaires_mois_2")) formData.append('horaires_mois_2', document.getElementById("horaires_mois_2").checked);
-    if(document.getElementById("horaires_mois_3")) formData.append('horaires_mois_3', document.getElementById("horaires_mois_3").checked);
-    if(document.getElementById("attestation")) formData.append('attestation', document.getElementById("attestation").checked);
-    if(document.getElementById("evaluation")) formData.append('evaluation', document.getElementById("evaluation").checked);
-    formData.append('relance', document.getElementById("relance").value);
-    
-    await fetch("../src/c/c_requetes.php", {
-      method: "POST",
-      body: formData
-    })
+  var formData = new FormData();
+  formData.append('envoyerMail', 1);
+  formData.append('id_stagiaire', document.getElementById("id_stagiaire").value);
+  formData.append('id_formateur', document.getElementById("id_formateur").value);
+  if (document.getElementById("convention")) formData.append('convention', document.getElementById("convention").checked);
+  if (document.getElementById("horaires_mois_1")) formData.append('horaires_mois_1', document.getElementById("horaires_mois_1").checked);
+  if (document.getElementById("horaires_mois_2")) formData.append('horaires_mois_2', document.getElementById("horaires_mois_2").checked);
+  if (document.getElementById("horaires_mois_3")) formData.append('horaires_mois_3', document.getElementById("horaires_mois_3").checked);
+  if (document.getElementById("attestation")) formData.append('attestation', document.getElementById("attestation").checked);
+  if (document.getElementById("evaluation")) formData.append('evaluation', document.getElementById("evaluation").checked);
+  formData.append('relance', document.getElementById("relance").value);
+
+  await fetch("../src/c/c_requetes.php", {
+    method: "POST",
+    body: formData
+  })
     .then((response) => response.json())
     .then((result) => {
       // window.location.reload();
     });
+}
+
+function exporterStages() {
+  if (document.getElementById("nom_session").value == 0) {
+    window.open('export.php', '_blank');
+  } else {
+    window.open('export.php?nom_session=' + document.getElementById("nom_session").value, '_blank');
+  }
 }

2e16cd01b340481d8a8854e01006d9de1d685fcc
MarceauAdrar <marceaurodrigues@adrar-formation.com>
2023-06-04 09:53:23 +0200

    Création d'une feuille d'export au format .xlsx

---
+++ public/export.php
@@ -0,0 +1,118 @@
+<?php
+error_reporting(E_ALL);
+ini_set("display_errors", 1);
+
+require __DIR__ . '/../src/vendor/autoload.php'; // Include the PhpSpreadsheet autoloader
+require __DIR__ . '/../src/m/connect.php';
+
+use PhpOffice\PhpSpreadsheet\Spreadsheet;
+use PhpOffice\PhpSpreadsheet\Writer\Xlsx;
+use PhpOffice\PhpSpreadsheet\Style\ConditionalFormatting\Wizard;
+use PhpOffice\PhpSpreadsheet\Style\Fill;
+use PhpOffice\PhpSpreadsheet\Style\Style;
+use PhpOffice\PhpSpreadsheet\Cell\DataValidation;
+
+// Création des couleurs
+$greenStyle = new Style(false, true);
+$greenStyle->getFill()
+  ->setFillType(Fill::FILL_SOLID)
+  ->getEndColor()->setARGB("A9D08E");
+// $greenStyle->getFont()->setColor(new Color(Color::COLOR_DARKRED));
+$redStyle = new Style(false, true);
+$redStyle->getFill()
+  ->setFillType(Fill::FILL_SOLID)
+  ->getEndColor()->setARGB("e77171");
+$redStyle->getFont()->setBold(true);
+
+$cellRangeChoix = 'C2:H1000';
+$choix = ['Oui', 'Non'];
+
+// Create a new Spreadsheet object
+$spreadsheet = new Spreadsheet();
+
+// Créer la validation des données avec la liste déroulante de choix
+$dataValidation = new DataValidation();
+$dataValidation->setType(DataValidation::TYPE_LIST);
+$dataValidation->setErrorStyle(DataValidation::STYLE_STOP);
+$dataValidation->setShowDropDown(true);
+$dataValidation->setFormula1('"' . implode(',', $choix) . '"');
+
+// Création des règles
+$conditionalStyles = [];
+$wizardFactory = new Wizard($cellRangeChoix);
+$cellWizard = $wizardFactory->newRule(Wizard::TEXT_VALUE);
+
+$cellWizard->contains("Oui")
+  ->setStyle($greenStyle);
+$conditionalStyles[] = $cellWizard->getConditional();
+
+$cellWizard->contains("Non")
+  ->setStyle($redStyle);
+$conditionalStyles[] = $cellWizard->getConditional();
+
+$req = $db->prepare("SELECT *
+                      FROM stagiaires
+                      JOIN sessions ON sessions.id_session = stagiaires.id_session
+                      WHERE nom_session " . (isset($_GET['nom_session']) && !empty($_GET['nom_session']) ? "=:nom_session" : "IS NOT NULL") . ";");
+if (isset($_GET['nom_session']) && !empty($_GET['nom_session'])) {
+  $req->bindParam(":nom_session", $_GET['nom_session']);
+}
+$req->execute();
+$session = $req->fetchAll(PDO::FETCH_ASSOC);
+
+if (!empty($session)) {
+  $sessions = array();
+  for ($stagiaire = 1; $stagiaire <= sizeof($session); $stagiaire++) {
+    if (!in_array($session[$stagiaire - 1]['nom_session'], $sessions)) {
+      $i = 2;
+      if(empty($sessions)) {
+        $sheet = $spreadsheet->getActiveSheet();
+      } else {
+        $sheet = $spreadsheet->createSheet();
+      }
+      array_push($sessions, $session[$stagiaire - 1]['nom_session']);
+      
+      $sheet->setTitle($session[$stagiaire - 1]['nom_session']);
+      
+      // Appliquer la validation des données à la plage de cellules spécifiée
+      $sheet->setDataValidation($cellRangeChoix, $dataValidation);
+
+      // Création de l'en-tête du tableau
+      $sheet->setCellValue('A1', 'NOM');
+      $sheet->setCellValue('B1', 'Prénom');
+      $sheet->setCellValue('C1', 'Convention de stage');
+      $sheet->setCellValue('D1', 'Horaires mois 1');
+      $sheet->setCellValue('E1', 'Horaires mois 2');
+      $sheet->setCellValue('F1', 'Horaires mois 3');
+      $sheet->setCellValue('G1', 'Attestation (envoyée/reçue)');
+      $sheet->setCellValue('H1', 'Évaluation (envoyée/reçue)');
+
+      // Ajuster automatiquement la taille des cellules en fonction du contenu
+      $sheet->getStyle('A2:H1000')->getAlignment()->setWrapText(true);
+
+      $sheet->getStyle($cellWizard->getCellRange())
+        ->setConditionalStyles($conditionalStyles);
+    }
+    $sheet->setCellValue("A" . $i, strtoupper($session[$stagiaire - 1]['nom_stagiaire']));
+    $sheet->setCellValue("B" . $i, ucwords($session[$stagiaire - 1]['prenom_stagiaire']));
+    $sheet->setCellValue("C" . $i, ($session[$stagiaire - 1]['convention_recue'] ? "Oui" : "Non"));
+    $sheet->setCellValue("D" . $i, ($session[$stagiaire - 1]['horaires_recues_1'] ? "Oui" : "Non"));
+    $sheet->setCellValue("E" . $i, ($session[$stagiaire - 1]['horaires_recues_2'] ? "Oui" : "Non"));
+    $sheet->setCellValue("F" . $i, ($session[$stagiaire - 1]['horaires_recues_3'] ? "Oui" : "Non"));
+    $sheet->setCellValue("G" . $i, ($session[$stagiaire - 1]['attestation_recue'] ? "Oui" : "Non"));
+    $sheet->setCellValue("H" . $i, ($session[$stagiaire - 1]['evaluation_recue'] ? "Oui" : "Non"));
+    $i++;
+  }
+}
+
+// Save the spreadsheet as XLSX
+$writer = new Xlsx($spreadsheet);
+// Set appropriate headers for file download
+header('Content-Type: application/vnd.openxmlformats-officedocument.spreadsheetml.sheet');
+header('Content-Disposition: attachment; filename="Liste.xlsx"');
+header('Cache-Control: max-age=0');
+
+$writer->save('php://output');
+exit;
+// $writer->save('output.xlsx'); // To save it on the server
+// echo 'Export completed.';

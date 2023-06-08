d63b3d6dc92c1fc6144ad45470b176d04e1309ef
Marceau <marceaurodrigues@adrar-formation.com>
2023-06-09 00:03:17 +0200

    [WIP] Formulaire de complétion

--- public/viewer.php
+++ public/viewer.php
@@ -39,13 +39,13 @@ if(!empty($pages)) {
         $html2pdf->AddPage();
         $html2pdf->Image(__DIR__ . '/../src/v/templates_documents/' . $document['index_document'] . '/' . $page['lien']);
         if (!empty($page['textes_ajoutes'])) {
-            foreach (json_decode($page['textes_ajoutes'], true) as $i) {
+            foreach (json_decode($page['textes_ajoutes'], true) as $key => $i) {
                 $html2pdf->setXY($i['X'], $i['Y']);
                 if (str_contains($i['texte'], "|")) {
                     $texte = explode('|', $i['texte']);
-                    if (array_key_exists($texte[0], $stage) || array_key_exists($texte[1], $stage)) {
+                    if (array_key_exists($texte[0], $stage) && array_key_exists($texte[1], $stage)) {
                         $html2pdf->Cell(0, 10, strtoupper($stage[$texte[0]]) . " " . ucwords($stage[$texte[1]]), 0, 1);
-                    } elseif (array_key_exists($texte[0], $formateur) || array_key_exists($texte[1], $formateur)) {
+                    } elseif (array_key_exists($texte[0], $formateur) && array_key_exists($texte[1], $formateur)) {
                         $html2pdf->Cell(0, 10, strtoupper($formateur[$texte[0]]) . " " . ucwords($formateur[$texte[1]]), 0, 1);
                     }
                 } elseif (array_key_exists($i['texte'], $stage)) {
@@ -64,6 +64,18 @@ if(!empty($pages)) {
                     ));
                     $html2pdf->Image(__DIR__ . "/../src/" . $i['signature'], $i['xsignature'], $i['ysignature'], 47, 27.5);
                 }
+            }
+        }
+        
+        // Ajout des informations saisies dans le formulaires si présentes
+        if(isset($_POST['document_attestation']) && !empty($_POST['document_attestation'])) {
+            if(isset($_POST['poste_occupe']) && !empty($_POST['poste_occupe'])) {
+                $html2pdf->setXY(50, 95.5);
+                $html2pdf->Cell(0, 10, filter_var($_POST['poste_occupe'], FILTER_SANITIZE_SPECIAL_CHARS), 0, 1);
+            }
+            if(isset($_POST['tache_effectuee_1']) && !empty($_POST['tache_effectuee_1'])) {
+                $html2pdf->setXY(25, 110);
+                $html2pdf->Cell(0, 10, filter_var($_POST['tache_effectuee_1'], FILTER_SANITIZE_SPECIAL_CHARS), 0, 1);
             }
         }
     }

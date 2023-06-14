fad89fa2038c95fe36f1394476274d3b14944d8c
Marceau <marceaurodrigues@adrar-formation.com>
2023-06-14 17:06:17 +0200

    Chargement de la feuille de style par défaut

--- public/stage/index.php
+++ public/stage/index.php
@@ -6,6 +6,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Automatisation documents stage</title>
+    <link rel="stylesheet" href="css/_style.css?v=<?= uniqid() ?>">
     <link rel="stylesheet" href="css/stage.css?v=<?= uniqid() ?>">
     <link rel="stylesheet" href="//cdn.datatables.net/1.13.4/css/jquery.dataTables.min.css">
     <script src="//code.jquery.com/jquery-3.5.1.js"></script>

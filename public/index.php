58dc0420974ebf23e386238cf2bbc11fd37959da
MarceauAdrar <marceaurodrigues@adrar-formation.com>
2023-06-18 23:58:19 +0200

    Ajout d'une page avec les différents choix disponibles

--- public/index.php
+++ public/index.php
@@ -1,8 +1,8 @@
 <?php
 include_once __DIR__ . '/m/connect.php';
 
-$page = "notfound.html";
 if(isset($_GET["page"]) && !empty($_GET["page"])) {
+    $page = "notfound.html";
     switch($_GET["page"]) {
         case "formation":
             $page = "formation/index.php";
@@ -17,7 +17,58 @@ if(isset($_GET["page"]) && !empty($_GET["page"])) {
             $page = "titre/index.php";
             break;
     }
+    include __DIR__."/".$page;die;
 } 
-
-include __DIR__."/".$page;
 ?>
+
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <script src="https://kit.fontawesome.com/b478fcca05.js" crossorigin="anonymous"></script>
+    <link rel="stylesheet" href="css/_style.css">
+    <title>Applications</title>
+</head>
+<body>
+    <div class="wrapper">
+        <div class="cards">
+            <div class="card">
+                <div class="card-title">Accueil</div>
+                <div class="card-body"></div>
+                <div class="card-footer">
+                    <a class="card-btn" href="?page=">Recharger</a>
+                </div>
+            </div>
+            <div class="card">
+                <div class="card-title">Administration</div>
+                <div class="card-body"></div>
+                <div class="card-footer">
+                    <a class="card-btn" href="?page=admin">Administrer</a>
+                </div>
+            </div>
+            <div class="card">
+                <div class="card-title">Formation</div>
+                <div class="card-body"></div>
+                <div class="card-footer">
+                    <a class="card-btn" href="?page=formation">WIP !</a>
+                </div>
+            </div>
+            <div class="card">
+                <div class="card-title">Stage</div>
+                <div class="card-body"></div>
+                <div class="card-footer">
+                    <a class="card-btn" href="?page=stage">Envoyer !</a>
+                </div>
+            </div>
+            <div class="card">
+                <div class="card-title">Titre</div>
+                <div class="card-body"></div>
+                <div class="card-footer">
+                    <a class="card-btn" href="?page=titre">Générer</a>
+                </div>
+            </div>
+        </div>
+    </div>
+</body>
+</html>

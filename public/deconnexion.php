22af980108f46aba355af79968fd46a5498be915
MarceauAdrar <marceaurodrigues@adrar-formation.com>
2023-06-27 22:40:56 +0200

    Redirection sur connexion si session expirée

--- public/deconnexion.php
+++ public/deconnexion.php
@@ -3,5 +3,5 @@
 session_start();
 session_destroy(); // Détruit les informations de la session
 unset($_SESSION); // Détruit les données stockées dans la variable de session
-header('Location: ./connexion.php')
+header('Location: ./connexion.php'.(isset($_GET['type']) && isset($_GET['message']) ? "?type=".$_GET['type']."&message=".$_GET['message'] : ""));
 ?>

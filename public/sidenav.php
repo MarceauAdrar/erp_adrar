2f8e029f5bedf92fda83bb7be0747f47eb60db59
MarceauAdrar <marceaurodrigues@adrar-formation.com>
2023-07-03 00:02:18 +0200

    Exportation de la sidenav

---
+++ public/sidenav.php
@@ -0,0 +1,32 @@
+<aside>
+    <div>
+        <img src="./img/logo_adrar.png" alt="Logo de l'ADRAR" style="width: 3em;">
+        &nbsp;<h1>erp adrar</h1>
+    </div>
+    <div>
+        <hr class="separator">
+        <nav>
+            <ul>
+                <li><a href="./"><i class="fa-solid fa-house fa-sm">&nbsp;</i><span>Accueil</span></a></li>
+                <li><a href="#" id="nav-administration"><i class="fa-solid fa-hammer fa-sm">&nbsp;</i><span>Administration</span></a></li>
+                <li><a href="#" id="nav-formation"><i class="fa-solid fa-book fa-sm">&nbsp;</i><span>Formation</span></a></li>
+                <li><a href="#" id="nav-stages"><i class="fa-solid fa-industry fa-sm">&nbsp;</i><span>Stages</span></a></li>
+                <li><a href="#" id="nav-titres"><i class="fa-solid fa-certificate fa-sm">&nbsp;</i><span>Titres</span></a></li>
+                <li><a href="#" id="nav-personal-folder"><i class="fa-solid fa-folder-open fa-sm">&nbsp;</i><span>Documents remis</span></a></li>
+            </ul>
+        </nav>
+    </div>
+    <div>
+        <hr class="separator">
+        <h2 class="d-block">Pages de comptes</h2>
+        <nav>
+            <ul>
+                <li><a href="./formulaire.php?form=mon-compte"><i class="fa-solid fa-user fa-sm">&nbsp;</i><span>Mon compte</span></a></li>
+                <li><a href="./deconnexion.php"><i class="fa-solid fa-arrow-right-from-bracket fa-sm">&nbsp;</i><span>Me déconnecter</span></a></li>
+            </ul>
+        </nav>
+    </div>
+    <div>
+        <small>&copy; ADRAR - Fait par Marceau RODRIGUES</small>
+    </div>
+</aside>

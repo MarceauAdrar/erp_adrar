baa1f4b2ac38bceb65431fbfe5929f446d07c965
MarceauAdrar <marceaurodrigues@adrar-formation.com>
2023-07-19 04:35:31 +0200

    Renommage du fichier

--- public/erreurs/404.php
+++ public/erreurs/404.php
@@ -7,15 +7,15 @@
     <meta name="theme-color" content="#ffffff"/>
     <meta name="description" content="ERP de l'ADRAR. Connectez-vous pour en voir plus.">
     <script src="https://kit.fontawesome.com/b478fcca05.js" crossorigin="anonymous"></script>
-    <link rel="stylesheet" href="css/_reset.css?v=<?= uniqid() ?>">
-    <link rel="stylesheet" href="css/_style.css?v=<?= uniqid() ?>">
+    <link rel="stylesheet" href="<?=$_SERVER['REQUEST_SCHEME']?>://<?=$_SERVER['SERVER_NAME']?>/erp/public/css/_reset.css?v=<?= uniqid() ?>">
+    <link rel="stylesheet" href="<?=$_SERVER['REQUEST_SCHEME']?>://<?=$_SERVER['SERVER_NAME']?>/erp/public/css/_style.css?v=<?= uniqid() ?>">
     <title>Accueil - ERP</title>
 </head>
 
 <body>
     <div class="container">
         <div class="sidenav">
-            <?= file_get_contents('sidenav.php') ?>
+            <?= file_get_contents('../sidenav.php') ?>
         </div>
         <div class="main">
             <h2>Erreur 404</h2>

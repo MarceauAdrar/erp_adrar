47bc70c26b2aa459b20949c1c727e621f8f47727
MarceauAdrar <marceaurodrigues@adrar-formation.com>
2023-07-19 04:36:26 +0200

    Changement des liens suite à l'ajout des ErrorDocument

--- public/sidenav.php
+++ public/sidenav.php
@@ -1,18 +1,18 @@
 <aside>
     <div>
-        <img src="./img/logo_adrar.png" alt="Logo de l'ADRAR" style="width: 3em;">
+        <img src="/erp/public/img/logo_adrar.png" alt="Logo de l'ADRAR" style="width: 3em;">
         &nbsp;<h1>erp adrar</h1>
     </div>
     <div>
         <hr class="separator">
         <nav>
             <ul>
-                <li><a href="./"><i class="fa-solid fa-house fa-sm">&nbsp;</i><span>Accueil</span></a></li>
-                <li><a href="?page=admin" id="nav-administration"><i class="fa-solid fa-hammer fa-sm">&nbsp;</i><span>Administration</span></a></li>
-                <li><a href="?page=formation" id="nav-formation"><i class="fa-solid fa-book fa-sm">&nbsp;</i><span>Formation</span></a></li>
-                <li><a href="?page=stage" id="nav-stages"><i class="fa-solid fa-industry fa-sm">&nbsp;</i><span>Stages</span></a></li>
-                <li><a href="?page=titre" id="nav-titres"><i class="fa-solid fa-certificate fa-sm">&nbsp;</i><span>Titres</span></a></li>
-                <li><a href="?page=boite-aux-lettres" id="nav-personal-folder"><i class="fa-solid fa-folder-open fa-sm">&nbsp;</i><span>Documents remis</span></a></li>
+                <li><a href="/erp/public"><i class="fa-solid fa-house fa-sm">&nbsp;</i><span>Accueil</span></a></li>
+                <li><a href="/erp/public?page=admin" id="nav-administration"><i class="fa-solid fa-hammer fa-sm">&nbsp;</i><span>Administration</span></a></li>
+                <li><a href="/erp/public?page=formation" id="nav-formation"><i class="fa-solid fa-book fa-sm">&nbsp;</i><span>Formation</span></a></li>
+                <li><a href="/erp/public?page=stage" id="nav-stages"><i class="fa-solid fa-industry fa-sm">&nbsp;</i><span>Stages</span></a></li>
+                <li><a href="/erp/public?page=titre" id="nav-titres"><i class="fa-solid fa-certificate fa-sm">&nbsp;</i><span>Titres</span></a></li>
+                <li><a href="/erp/public?page=boite-aux-lettres" id="nav-personal-folder"><i class="fa-solid fa-folder-open fa-sm">&nbsp;</i><span>Documents remis</span></a></li>
             </ul>
         </nav>
     </div>
@@ -21,8 +21,8 @@
         <h2 class="d-block">Pages de comptes</h2>
         <nav>
             <ul>
-                <li><a href="./formulaire.php?form=mon-compte"><i class="fa-solid fa-user fa-sm">&nbsp;</i><span>Mon compte</span></a></li>
-                <li><a href="./deconnexion.php"><i class="fa-solid fa-arrow-right-from-bracket fa-sm">&nbsp;</i><span>Me déconnecter</span></a></li>
+                <li><a href="/erp/public/formulaire.php?form=mon-compte"><i class="fa-solid fa-user fa-sm">&nbsp;</i><span>Mon compte</span></a></li>
+                <li><a href="/erp/public/deconnexion.php"><i class="fa-solid fa-arrow-right-from-bracket fa-sm">&nbsp;</i><span>Me déconnecter</span></a></li>
             </ul>
         </nav>
     </div>

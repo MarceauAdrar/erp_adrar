eca23b08a38f940f926b41a849bdec4c3489901a
MarceauAdrar <marceaurodrigues@adrar-formation.com>
2023-10-06 17:09:20 +0000

    Changement du lien

--- public/formation/error404.php
+++ public/formation/error404.php
@@ -1,1 +1,1 @@
-<img class="img-center" style="width: 50vw; height: 75vh;" src="/public/imgs/not_found_404.svg" alt="Illustration pour une page non trouvée">
+<img class="img-center" style="width: 50vw; height: 75vh;" src="/erp/public/formation/imgs/not_found_404.svg" alt="Illustration pour une page non trouvée">

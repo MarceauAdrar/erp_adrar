eb86194ffff53e2f056b2eca875570428ab11aff
MarceauAdrar <marceaurodrigues@adrar-formation.com>
2023-11-13 19:01:08 +0000

    Retrait de la méthode "https"

--- public/formation/js.php
+++ public/formation/js.php
@@ -1,7 +1,7 @@
         <!-- JavaScript Bundle with Popper -->
         <script src="<?= $_SERVER["REQUEST_SCHEME"] ?>://<?= $_SERVER["SERVER_NAME"] ?>/erp/public/formation/js/bootstrap.bundle.min.js"></script>
         <script src="//code.jquery.com/jquery-3.6.2.min.js" integrity="sha256-2krYZKh//PcchRtd+H+VyyQoZ/e3EcrkxhM8ycwASPA=" crossorigin="anonymous"></script>
-	<script src="https://cdn.jsdelivr.net/npm/sortablejs@latest/Sortable.min.js"></script>
+        <script src="//cdn.jsdelivr.net/npm/sortablejs@latest/Sortable.min.js" crossorigin="anonymous"></script>
         <script src="<?= $_SERVER["REQUEST_SCHEME"] ?>://<?= $_SERVER["SERVER_NAME"] ?>/erp/public/formation/js/consts.js?v=<?= uniqid() ?>" type="text/javascript"></script>
         <script src="<?= $_SERVER["REQUEST_SCHEME"] ?>://<?= $_SERVER["SERVER_NAME"] ?>/erp/public/formation/js/index.js?v=<?= uniqid() ?>" type="text/javascript"></script>
         <script src="<?= $_SERVER["REQUEST_SCHEME"] ?>://<?= $_SERVER["SERVER_NAME"] ?>/erp/public/formation/js/tooltips.js?v=<?= uniqid() ?>" type="text/javascript"></script>

010c21be739b5564c5d1cc83dc5ada41de1ef7be
MarceauAdrar <marceaurodrigues@adrar-formation.com>
2023-11-30 10:53:29 +0000

    Ajout ECF CDA

--- public/js/_global.js
+++ public/js/_global.js
@@ -3,7 +3,7 @@ function updateForm() {
     documents.forEach(doc => {
         document.querySelector('#' + doc).classList.add('hidden');
     });
-    switch(document.querySelector('#document').value) {
+    switch (document.querySelector('#document').value) {
         case "convention_no_logo":
         case "convention_region":
         case "convention_pe":
@@ -24,17 +24,21 @@ function updateForm() {
             document.querySelector('#document_presence').classList.toggle('hidden');
             document.querySelector('#document_informations').setAttribute('name', "document_presence");
             break;
-        case "livret_evaluation":
+        case "livret_evaluation/dwwm/":
             document.querySelector('#document_livret_evaluation').classList.toggle('hidden');
-            document.querySelector('#document_informations').setAttribute('name', "document_livret_evaluation");
+            document.querySelector('#document_informations').setAttribute('name', "document_livret_evaluation/dwwm/");
+            break;
+        case "livret_evaluation/cda/":
+            document.querySelector('#document_livret_evaluation').classList.toggle('hidden');
+            document.querySelector('#document_informations').setAttribute('name', "document_livret_evaluation/cda/");
             break;
     }
 }
 
 function loadNextInput(doc, input, tache_suivante) {
-    if(input.value.trim() !== '') { 
-        document.querySelector('#' + doc + parseInt(tache_suivante)).parentElement.classList.remove('hidden'); 
-    } else { 
+    if (input.value.trim() !== '') {
+        document.querySelector('#' + doc + parseInt(tache_suivante)).parentElement.classList.remove('hidden');
+    } else {
         document.querySelectorAll('.' + doc).forEach(element => {
             document.querySelector('#' + doc + parseInt(tache_suivante)).parentElement.classList.add('hidden');
         });
